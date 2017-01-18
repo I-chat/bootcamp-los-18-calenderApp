45e2119c98ca97c4dbfaa2cba147204531d84f8d
Ichiato Ikikin <ichiatoikikin@gmail.com>
2017-01-18 23:39:14 +0100

    Update working month view

--- switch.js
+++ switch.js
@@ -1,32 +1,43 @@
-var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var date = new Date();
-var firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+var next = document.getElementById('next');
+var prev = document.getElementById('prev');
+var table = document.getElementById('day').getElementsByTagName('td');
 
-document.getElementById("label").innerHTML = `${month[date.getMonth()]} ${date.getFullYear()}`
-document.getElementById('next').addEventListener("click", function(){
-    document.getElementById("label").innerHTML = setMonth(date, true);
-})
-document.getElementById('prev').addEventListener("click", function(){
-    document.getElementById("label").innerHTML = setMonth(date, false);
-})
-var setMonth = function (currentDate, movement) {
+document.getElementById("label").innerHTML = `${months[date.getMonth()]} ${date.getFullYear()}`
+next.addEventListener("click", function(){document.getElementById("label").innerHTML = setMonths(date, true)}, false);
+prev.addEventListener("click", function(){document.getElementById("label").innerHTML = setMonths(date, false)}, false);
+table.addEventListener("load", setDays(date));
+
+function setMonths(currentDate, movement) {
 	if (movement === false) {
 		date = new Date(currentDate.getFullYear(), currentDate.getMonth()-1, 1);
-		return `${month[date.getMonth()]} ${date.getFullYear()}`;
+		for (var i = 0; i < table.length; ++i) {
+			table[i].innerHTML = "";
+		};
+		setDays(date);
+		return `${months[date.getMonth()]} ${date.getFullYear()}`;
 	} else {
 		date = new Date(currentDate.getFullYear(), currentDate.getMonth()+1, 1);
-		return `${month[date.getMonth()]} ${date.getFullYear()}`
+		for (var i = 0; i < table.length; ++i) {
+			table[i].innerHTML = "";
+		};
+		setDays(date);
+		return `${months[date.getMonth()]} ${date.getFullYear()}`
 	}
 }
 
-var daysInMonth = function(month, year) {
-	return new Date(year, month, 0).getDate();
+function daysInMonth(month, year) {
+	return new Date(year, month+1, 0).getDate();
 }
-var setDays = function(currentDate = date, movement) {
-	var table = document.getElementById('day').getElementsByTagName('td');
-	for (var i = firstDayOfMonth, cell; cell = table[i]; i++) {
-		if (i < daysInMonth(date.getMonth(), date.getFullYear())) {
-			cell.innerHTML = i + 1;
-		}
+
+function setDays(currentDate) {
+	console.log(currentDate.getMonth());
+	var noOfDays = daysInMonth(currentDate.getMonth(), currentDate.getFullYear());
+	console.log(noOfDays);
+	var firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+	console.log(firstDayOfMonth);
+	for (var i = firstDayOfMonth, k=0; k < noOfDays; i++, k++) {
+		table[i].innerHTML = k + 1;
 	}
-}()
+}
