dd678d53da06082b12d0cee2f7d5acfa6a025570
Ichiato Ikikin <ichiatoikikin@gmail.com>
2017-01-18 13:13:54 +0100

    add setDays function

--- switch.js
+++ switch.js
@@ -1,29 +1,32 @@
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var date = new Date();
-var JsCalender = function () {
-	var switchMonth = function (currentDate, movement) {
-		if (movement === false) {
-			date = new Date(currentDate.getFullYear(), currentDate.getMonth()-1, 1);
-			return `${month[date.getMonth()]} ${date.getFullYear()}`;
-		} else {
-			date = new Date(currentDate.getFullYear(), currentDate.getMonth()+1, 1);
-			return `${month[date.getMonth()]} ${date.getFullYear()}`
-		}
+var firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+
+document.getElementById("label").innerHTML = `${month[date.getMonth()]} ${date.getFullYear()}`
+document.getElementById('next').addEventListener("click", function(){
+    document.getElementById("label").innerHTML = setMonth(date, true);
+})
+document.getElementById('prev').addEventListener("click", function(){
+    document.getElementById("label").innerHTML = setMonth(date, false);
+})
+var setMonth = function (currentDate, movement) {
+	if (movement === false) {
+		date = new Date(currentDate.getFullYear(), currentDate.getMonth()-1, 1);
+		return `${month[date.getMonth()]} ${date.getFullYear()}`;
+	} else {
+		date = new Date(currentDate.getFullYear(), currentDate.getMonth()+1, 1);
 		return `${month[date.getMonth()]} ${date.getFullYear()}`
 	}
-	document.getElementById('next').addEventListener("click", function(){
-    	document.getElementById("label").innerHTML = switchMonth(date, true);
-	})
-	document.getElementById('prev').addEventListener("click", function(){
-    	document.getElementById("label").innerHTML = switchMonth(date, false);
-	})
-}()
-var setMonth = function() {
-	document.getElementById("label").innerHTML = `${month[date.getMonth()]} ${date.getFullYear()}`
-}()
+}
+
 var daysInMonth = function(month, year) {
 	return new Date(year, month, 0).getDate();
 }
-var setDay = function() {
-	document.getElementById
-}
+var setDays = function(currentDate = date, movement) {
+	var table = document.getElementById('day').getElementsByTagName('td');
+	for (var i = firstDayOfMonth, cell; cell = table[i]; i++) {
+		if (i < daysInMonth(date.getMonth(), date.getFullYear())) {
+			cell.innerHTML = i + 1;
+		}
+	}
+}()
