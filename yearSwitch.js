95aa0acfe945d911aa660f7baeb42d02c96931c5
Ichiato Ikikin <ichiatoikikin@gmail.com>
2017-01-19 10:59:14 +0100

    Update year list view

--- yearSwitch.js
+++ yearSwitch.js
@@ -2,6 +2,6 @@ var date = new Date(1980, 0);
 var btn = document.querySelectorAll("td");
 
 for (var i = 0; i < btn.length; i++){
-	btn[i].innerHTML = date.getFullYear() + i;
+	btn[i].innerHTML = `<a href="listmonth.html"> ${date.getFullYear() + i} </a>`;
 }
 console.log(btn)
